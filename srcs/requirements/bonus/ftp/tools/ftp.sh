c846df60a9f644a96eeea571ebdda307f28fbd5f
Jirayu Kaewsing <strixz.self@gmail.com>
2025-05-12 11:50:30 +0700

    Fix FTP permission

--- srcs/requirements/bonus/ftp/tools/ftp.sh
+++ srcs/requirements/bonus/ftp/tools/ftp.sh
@@ -13,9 +13,12 @@ envsubst < /etc/vsftpd.conf.template > /etc/vsftpd.conf
 if ! id "$FTP_USER" &>/dev/null; then
     adduser --disabled-password --gecos "" "$FTP_USER"
     echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
-    chown -R $FTP_USER:$FTP_USER /var/www/html
-    chmod -R 755 /var/www/html
+    usermod -aG www-data $FTP_USER
 fi
+
+chown -R $FTP_USER:$FTP_USER /var/www/html
+chmod -R 775 /var/www/html
+chmod g+s /var/www/html
 
 echo "$FTP_USER" | tee -a /etc/vsftpd.userlist > /etc/vsftpd.chroot_list
 usermod -d /var/www/html $FTP_USER
